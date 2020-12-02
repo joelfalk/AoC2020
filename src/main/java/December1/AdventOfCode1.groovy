8f7b968aa47a513e011e2c3c98fb7eca1d9edcc2
Joel Falk <joel.falk@evry.com>
2020-12-02 19:57:15 +0100

    added bad solution of part 2

--- src/main/java/December1/AdventOfCode1.groovy
+++ src/main/java/December1/AdventOfCode1.groovy
@@ -2,7 +2,7 @@ package December1
 
 def startTime = System.currentTimeMillis()
 
-/*
+    /*
      fill up ArrayList with values.
      */
 def list = new ArrayList<Integer>()
@@ -13,14 +13,31 @@ content.eachLine {line ->
 content.close()
 
     /*
+    Part 1:
     for each index in list, see if valid value to sum index + valid value = 2020 exists.
     if exists, print the product of index + valid value
     */
 for (index in list) {
     if (list.contains(2020 - index)) {
-        println index*(2020-index)
+        println "Part 1: " + (index * (2020 - index))
         break
     }
 }
+
+    /*
+    Part 2:
+        nested for-loops. Not good.
+    */
+def part2
+for (value1 in list) {
+    for (value2 in list){
+        def value3 = 2020 - (value1 + value2)
+        if(list.contains(value3)){
+            part2 = (value1 * value2 * value3)
+            break
+        }
+    }
+}
+println "Part 2: " + part2
 
 println "total time " + (System.currentTimeMillis() - startTime)
