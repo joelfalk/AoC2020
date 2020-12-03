ab7c1352d2441d970306eb335367aa3c2ce0a093
Joel Falk <joel.falk@evry.com>
2020-12-03 21:06:49 +0100

    refactored code, changed name of result variable to result

--- src/main/java/December2/AdventOfCode2.groovy
+++ src/main/java/December2/AdventOfCode2.groovy
@@ -2,22 +2,22 @@
     fill upp array with list of strings from input data
     which was split row by row by "-", ": " and " ".
     This in each row as an list of:  [ MIN, MAX, CHAR, PASSWORD ]
- */
 
-def content = new File("AdventOfCode2").newInputStream()
-ArrayList<List<String>> list = new ArrayList<List<String>>()
-content.splitEachLine('-|:+\\s|\\s', { line -> list.add(line) })
-
-/*
     for each row, count if PASSWORD contains CHAR
     at least MIN times and at most MAX times.
  */
-def count = 0
+def result = 0
 
-list.each { row ->
-    if (row[3].count(row[2]) >= row[0].toInteger() && row[3].count(row[2]) <= row[1].toInteger()) {
-        count++
-    }
-}
+def content = new File("AdventOfCode2").newInputStream()
 
-print(count)
+content.splitEachLine('-|:+\\s|\\s',
+        { row ->
+            if (row[3].count(row[2]) >= row[0].toInteger() && row[3].count(row[2]) <= row[1].toInteger()) {
+                result++
+            }
+        }
+)
+
+print(result)
+
+
