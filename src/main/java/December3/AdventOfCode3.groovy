d4590e4d1f82d7c7dd3c3ee2b867b0d3187199b0
Joel Falk <joel.falk@evry.com>
2020-12-04 12:26:11 +0100

    changed solution to fit both part 1 and part 2

--- src/main/java/December3/AdventOfCode3.groovy
+++ src/main/java/December3/AdventOfCode3.groovy
@@ -1,23 +1,52 @@
+import com.sun.xml.internal.ws.wsdl.writer.document.Part
+
 /*
     PART 1:
     read input data. for each row (skip first row)
     check for # symbolizing a tree at coordinate x.
     if tree -> result ++
+
+    PART 2:
+    Do part1 but for different x and y. Refactored to
+    manually set stepsX and stepsY. Use modulo to check
+    if stepsX should be active.
  */
-def content = new File('AdventOfCode3').newInputStream()
-def x = 0
-def result = 0
 
-content.eachLine( { row ->
-    if (row.charAt(x) == '#') {
-        result++
+
+private int treeCounter(int stepsX, int stepsY) {
+    def content = new File('AdventOfCode3').newInputStream()
+    def x = stepsX
+    def result = 0
+
+    content.eachLine { row, y ->
+        if (y == 1) {
+            return
+        }
+
+        if ((y + 1) % stepsY == 0) {
+            if (row.charAt(x) == '#') {
+                result++
+            }
+
+            if (stepsX >= (row.size() - x)) {
+                x = (x - row.size()) + stepsX
+            } else {
+                x += stepsX
+            }
+        }
     }
-    if (x >= 28) {
-        x = (x - 31) + 3
-    } else {
-        x += 3
-    }
+    return result
+
 }
-)
 
-println(result)
+
+static void main(String[] args) {
+
+    println(treeCounter(3,1))
+    println(treeCounter(1,1))
+    println(treeCounter(5,1))
+    println(treeCounter(7,1))
+    println(treeCounter(1, 2))
+
+
+}
