ecd62f1ff05ff9be05db5cb9357621caecbb745a
Joel Falk <joel.falk@evry.com>
2020-12-06 16:46:19 +0100

    added comments for dec4 part1

--- src/main/java/December4/AdventOfCode4.groovy
+++ src/main/java/December4/AdventOfCode4.groovy
@@ -2,6 +2,11 @@ import java.util.stream.Collectors
 
 def content = new File('AdventOfCode4').newInputStream()
 
+/*
+    Added input data to a map. Add each line as value to same
+    key until blank line, then add row to key++.
+    Each key will represent a passport.
+ */
 Map<Integer, List<String>> map = new HashMap<>()
 def key = 0
 content.splitEachLine('\\s',
@@ -20,17 +25,26 @@ content.splitEachLine('\\s',
         }
 )
 
-private static boolean validPassportFields(List<String> list) {
-    List condition = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-
-    return  list.containsAll(condition)
-}
-
-
+/*
+    use Java-streams to only use field identifier,
+    then filter out the invlaidPassports by
+    comfirming passport contains All of the
+    required fields: ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+ */
 long s = map.values().stream()
-        .map({ fields -> fields.stream().map({ field -> field.substring(0, 3)}).collect(Collectors.toList()) })
-        .filter({passports -> validPassportFields(passports)})
+        .map({ fields ->
+            fields.stream()
+                    .map({ field -> field.substring(0, 3) })
+                    .collect(Collectors.toList())
+                    }
+            )
+        .filter({ passports -> validPassportFields(passports) })
         .count()
 
+
+private static boolean validPassportFields(List<String> list) {
+    List reqFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+    return list.containsAll(reqFields)
+}
 
 println("Part 1: " + s)
